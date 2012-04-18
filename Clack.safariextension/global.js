0fef8c6770773c2cc65ec98160702a1fc743f238
canisbos <canisbos@gmail.com>
2012-04-18 10:08:58 -0700

    simplified flipIcon function; converted tabs to spaces

--- Clack.safariextension/global.js
+++ Clack.safariextension/global.js
@@ -1,29 +1,25 @@
 function flipIcon() {
-	safari.extension.toolbarItems.forEach(function (ti) {
-		if (ti.image.match('left.png')) {
-			ti.image = safari.extension.baseURI + 'right.png';
-		} else {
-			ti.image = safari.extension.baseURI + 'left.png';
-		}
-	});
+    safari.extension.toolbarItems.forEach(function (ti) {
+        ti.image = safari.extension.baseURI + (ti.image.match('left.png') ? 'right' : 'left') + '.png';
+    });
 }
 function handleCommand(e) {
-	if (e.command === 'flipOther') {
-		safari.application.activeBrowserWindow.activeTab.page.dispatchMessage('sendRequest', {
-			request: 'flipIcon',
-			origin: safari.extension.baseURI,
-			data: null
-		});
-	}
+    if (e.command === 'flipOther') {
+        safari.application.activeBrowserWindow.activeTab.page.dispatchMessage('sendRequest', {
+            request: 'flipIcon',
+            origin: safari.extension.baseURI,
+            data: null
+        });
+    }
 }
 function handleMessage(e) {
-	if (e.name === 'handleRequest') {
-		if (e.message.origin !== safari.extension.baseURI) {
-			if (e.message.request === 'flipIcon') {
-				flipIcon();
-			}
-		}
-	}
+    if (e.name === 'handleRequest') {
+        if (e.message.origin !== safari.extension.baseURI) {
+            if (e.message.request === 'flipIcon') {
+                flipIcon();
+            }
+        }
+    }
 }
 
 safari.application.addEventListener('command', handleCommand, false);
--- Click.safariextension/injected.js
+++ Click.safariextension/injected.js
@@ -1,12 +1,12 @@
 function handleInternalMessage(e) {
-	if (e.name === 'sendRequest') {
-		window.postMessage(e.message, '*');
-	}
+    if (e.name === 'sendRequest') {
+        window.postMessage(e.message, '*');
+    }
 }
 function handleExternalMessage(e) {
-	if (e.origin === location.protocol + '//' + location.host) {
-		safari.self.tab.dispatchMessage('handleRequest', e.data);
-	}
+    if (e.origin === location.protocol + '//' + location.host) {
+        safari.self.tab.dispatchMessage('handleRequest', e.data);
+    }
 }
 
 safari.self.addEventListener('message', handleInternalMessage, false);
